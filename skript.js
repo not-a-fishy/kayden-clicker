2fadec820ac02565c10e338cd20b1c7a087f008e
not-a-fishy <gao_yuanbin@students.edu.sg>
2026-09-14 16:25:19 +0800

    Add upgrade functionality and refactor event listener

--- skript.js
+++ skript.js
@@ -1,19 +1,44 @@
 let kdnscore = 0;
 let clickstr = 1;
 let passivekdn = 0;
+let upgamt = ["NA", 0]
 
+const upgrades = [
+    "this text is here to distract you why are u looking at it",
+    {
+        upgrade: "1A3",
+        cost: 20,
+        normalinc: 1,
+        clickinc: 0,
+        limit: 32,
+        flavourText: "You manage to get a whole class pissed off against one guy. Wow... (Upgrade Limit Reached) "
+    }
+]
 const button = document.getElementById("kdn");
 const clicktrack = document.getElementById("m");
 const auto = document.getElementById("a");
 const score = document.getElementById("scor");
 const button1 = document.getElementById("upg1");
+const ui1 = document.getElementById("uppie1");
 
 button.addEventListener('click', hi);
-button1.addEventListener('click', tryupgrade);
+button1.addEventListener('click', () => upgrade(1));
 
-function tryupgrade(){
-    alert("trying to upgrade!")
+function upgrade(type){
+    if (kdnscore >= upgrades[type]["cost"] && upgamt[type]<upgrades[type]["limit"]){ 
+        kdnscore -= upgrades[type]["cost"];
+        passivekdn += upgrades[type]["normalinc"];
+        clickstr += upgrades[type]["clickinc"];
+        upgamt[type] += 1;
+    } else if (upgamt[type]>=upgrades[type]["limit"]) {
+        alert(upgrades[type]["flavourText"]);
+    } else {
+        alert("Not enough Kayden Points!");
+    }
+    ui1.innerText = upgamt[1];
+
 }
+
 
 function hi(){
     kdnscore += clickstr;
